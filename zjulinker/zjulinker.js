63b57f593dfd688161cf4fdbb39c2de96ef4f2a8
dongmingsun <dongming.sun@outlook.com>
2017-04-25 18:24:34 +0800

    Site updated: 2017-04-25 18:24:34

--- zjulinker/zjulinker.js
+++ zjulinker/zjulinker.js
@@ -1,4 +1,4 @@
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css">/**
+/**
  * Created by dongming on 2017/4/25.
  */
 function sliderPindan() {
